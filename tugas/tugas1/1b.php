c3fe613b7b50f5101eccff927492a30fdd767396
nadaaputri@gmail.com <putriaqilah05@gmail.com>
2024-02-20 08:42:07 +0700

    First Commit

---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,22 @@
+<?php
+// menetapkan nilai
+$x = 38;
+
+echo "Aku adalah angka <b>$x</b> </br>";
+
+// perkalian
+$hasilKali = $x * 5;
+echo "Jika aku dikali 5, maka aku sekarang menjadi <b>$hasilKali</b> </br>";
+
+// pembagian
+$hasilBagi = $x / 2;
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <b>$hasilBagi</b> </br>"; 
+
+// penjumlahan
+$hasilJumlah = $x + 75;
+echo "Jika aku ditambah 75, maka aku sekarang menjadi <b>$hasilJumlah</b> </br>";
+
+// pengurangan 
+$hasilKurang = $x - 20;
+echo "Jika aku dikurang 20, maka aku sekarang menjadi <b>$hasilKurang</b> </br>";
+?>
